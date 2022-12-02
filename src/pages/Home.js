4a58e692bb93b8165b27d3556d78f2450af1d71d
Felipe Rivera <felipe.rivera@uc.cl>
2022-12-02 16:45:26 -0300

    feat: mensajes de formato

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -105,11 +105,11 @@ function Home() {
                 </h1>
                 <form className="space-y-4 md:space-y-6" action="#">
                     <div>
-                        <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Rut</label>
+                        <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Rut (sin puntos y con guion)</label>
                         <input type="text" placeholder="Rut" onChange={handleRut} value={rut} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required="" />
                     </div>
                     <div>
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Numero de documento</label>
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Numero de documento (sin puntos)</label>
                         <input type="text" placeholder="Numero de documento" onChange={handleDocnum} value={docnum} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required="" />
                     </div>
                     <div className="flex items-center justify-between">
