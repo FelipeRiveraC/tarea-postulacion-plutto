c469eba2cf96ff6cb9e0225e76921102679337b1
Felipe Rivera <felipe.rivera@uc.cl>
2022-12-02 15:13:04 -0300

    feat: basic functionality added

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+  <div className='ml-[max(10vw,140px)] mt-24 mr-[max(6vw,12px)]'>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </div>
+  </BrowserRouter>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
